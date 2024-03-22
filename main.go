347c01096f715edc3cf18bf2d31ff978127d7c88
Kanaz312 <mtsu.kana@gmail.com>
2024-03-21 21:36:28 -0500

    Implemented scenario info data
    Added struct to hold scenario data and struct to hold decision outcomes
    Updated game states and server state to use these new structs
    Updated endpoints to use the decision outcome data

--- main.go
+++ main.go
@@ -7,22 +7,40 @@ import (
 	"net/http"
 )
 
+type decision struct {
+	Cross int;
+	Population int;
+	Sword int;
+	Money int;
+	Description string;
+
+}
+
+type scenario struct {
+	Prompt string;
+	Image string;
+	LeftDecision decision;
+	RightDecision decision;
+}
+
 type gameState struct {
 	Cross int;
 	Population int;
 	Sword int;
 	Money int;
+	Scenario scenario;
 }
 
 type serverState struct {
-	gameStates []gameState;
+	GameStates []gameState;
+	Scenarios []scenario;
 }
 
 
 var indexTpl = template.Must(template.ParseFiles("index.html"))
 
 func (s *serverState) indexHandler(w http.ResponseWriter, r *http.Request) {
-	err := indexTpl.Execute(w, s.gameStates[0])
+	err := indexTpl.Execute(w, s.GameStates[0])
 	if err != nil {
 		fmt.Printf("Failed to execute index.html %v\n", err)
 	}
@@ -31,40 +49,55 @@ func (s *serverState) indexHandler(w http.ResponseWriter, r *http.Request) {
 var statTpl = template.Must(template.ParseFiles("assets/static/stats.html"))
 
 func (s *serverState) statsHandler(w http.ResponseWriter, r *http.Request) {
-	err := statTpl.Execute(w, s.gameStates[0])
+	err := statTpl.Execute(w, s.GameStates[0])
 	if err != nil {
 		fmt.Printf("Failed to execute assets/static/stats.html %v\n", err)
 	}
 }
 
 func (s *serverState) leftHandler(w http.ResponseWriter, r *http.Request) {
-	gameState := &s.gameStates[0]
-	gameState.Cross += 20
-	gameState.Population -= 20
-	gameState.Sword += 20
-	gameState.Money -= 20
+	currentGameState := &s.GameStates[0]
+	currentScenario := currentGameState.Scenario
+	
+	currentGameState.Cross += currentScenario.LeftDecision.Cross
+	currentGameState.Population += currentScenario.LeftDecision.Population
+	currentGameState.Sword += currentScenario.LeftDecision.Sword
+	currentGameState.Money += currentScenario.LeftDecision.Money
 
 	w.WriteHeader(http.StatusAccepted)
 	w.Write([]byte("200 - Left Received"))
-	log.Printf("Handled left, new stats: %d %d %d %d\n", gameState.Cross, gameState.Population, gameState.Sword, gameState.Money)
+	log.Printf("Handled left, new stats: %d %d %d %d\n", currentGameState.Cross, currentGameState.Population, currentGameState.Sword, currentGameState.Money)
 }
 
 func (s *serverState) rightHandler(w http.ResponseWriter, r *http.Request) {
-	gameState := &s.gameStates[0]
-	gameState.Cross -= 20
-	gameState.Population += 20
-	gameState.Sword -= 20
-	gameState.Money += 20
+	currentGameState := &s.GameStates[0]
+	currentScenario := currentGameState.Scenario
+	
+	currentGameState.Cross += currentScenario.RightDecision.Cross
+	currentGameState.Population += currentScenario.RightDecision.Population
+	currentGameState.Sword += currentScenario.RightDecision.Sword
+	currentGameState.Money += currentScenario.RightDecision.Money
 
 	w.WriteHeader(http.StatusAccepted)
 	w.Write([]byte("200 - Right Received"))
-	log.Printf("Handled right, new stats: %d %d %d %d\n", gameState.Cross, gameState.Population, gameState.Sword, gameState.Money)
+	log.Printf("Handled right, new stats: %d %d %d %d\n", currentGameState.Cross, currentGameState.Population, currentGameState.Sword, currentGameState.Money)
 }
 
 func main() {
-	mainServerState := serverState{make([]gameState, 5)}
-	state := gameState{50, 50, 50, 50}
-	mainServerState.gameStates[0] = state;
+	left0 := decision{30, 10, 0, -40, "Build the church"}
+	right0 := decision{-20, 0, 0, 10, "No good, costs too much"}
+	scenario0 := scenario{"We would like to construct a cathedral to spread the Good Word", "jack", left0, right0}
+	left1 := decision{-10, 40, 0, -10, "Clear the forests to make new farmland"}
+	right1 := decision{0, 30, -30, 0, "Steal from our neighbors"}
+	scenario1 := scenario{"The people are going hungry", "queen", left1, right1}
+
+	mainServerState := serverState{make([]gameState, 5), make([]scenario, 2)}
+
+	mainServerState.Scenarios[0] = scenario0
+	mainServerState.Scenarios[1] = scenario1
+
+	sampleState := gameState{50, 50, 50, 50, scenario0}
+	mainServerState.GameStates[0] = sampleState ;
 
 	fs := http.FileServer(http.Dir("assets"))
 
